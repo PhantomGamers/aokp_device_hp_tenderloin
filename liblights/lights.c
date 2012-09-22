f85bfa939a6b3fd7a80f39b0b1ba225446df9709
PhantomGamers <phantom@sundergaming.com>
2012-09-22 19:27:30 -0400

    more LOG -> ALOG

--- liblights/lights.c
+++ liblights/lights.c
@@ -82,7 +82,7 @@ static int write_int(char const *path, int value)
 
 	already_warned = 0;
 
-	LOGV("write_int: path %s, value %d", path, value);
+	ALOGV("write_int: path %s, value %d", path, value);
 	fd = open(path, O_RDWR);
 
 	if (fd >= 0) {
@@ -93,7 +93,7 @@ static int write_int(char const *path, int value)
 		return amt == -1 ? -errno : 0;
 	} else {
 		if (already_warned == 0) {
-			LOGE("write_int failed to open %s\n", path);
+			ALOGE("write_int failed to open %s\n", path);
 			already_warned = 1;
 		}
 		return -errno;
@@ -117,20 +117,20 @@ static void init_notification_led(void)
 
 	fd = open(LED_FILE, O_RDWR);
 	if (fd < 0) {
-		LOGE("Cannot open notification LED device - %d", errno);
+		ALOGE("Cannot open notification LED device - %d", errno);
 		return;
 	}
 
 	/* download microcode */
 	if (ioctl(fd, LM8502_DOWNLOAD_MICROCODE, microcode) < 0) {
-		LOGE("Cannot download LED microcode - %d", errno);
+		ALOGE("Cannot download LED microcode - %d", errno);
 		return;
 	}
 	if (ioctl(fd, LM8502_STOP_ENGINE, 1) < 0) {
-		LOGE("Cannot stop LED engine 1 - %d", errno);
+		ALOGE("Cannot stop LED engine 1 - %d", errno);
 	}
 	if (ioctl(fd, LM8502_STOP_ENGINE, 2) < 0) {
-		LOGE("Cannot stop LED engine 2 - %d", errno);
+		ALOGE("Cannot stop LED engine 2 - %d", errno);
 	}
 
 	close(fd);
@@ -143,7 +143,7 @@ static int set_light_notifications(struct light_device_t* dev,
 	uint16_t microcode[96];
 	int fd, ret;
 
-	LOGI("%sabling notification light", on ? "En" : "Dis");
+	ALOGI("%sabling notification light", on ? "En" : "Dis");
 	memset(microcode, 0, sizeof(microcode));
 	if (on) {
 		memcpy(microcode, notif_led_program_pulse, sizeof(notif_led_program_pulse));
@@ -155,48 +155,48 @@ static int set_light_notifications(struct light_device_t* dev,
 
 	fd = open(LED_FILE, O_RDWR);
 	if (fd < 0) {
-		LOGE("Opening %s failed - %d", LED_FILE, errno);
+		ALOGE("Opening %s failed - %d", LED_FILE, errno);
 		ret = -errno;
 	} else {
 		ret = 0;
 
 		/* download microcode */
 		if (ioctl(fd, LM8502_DOWNLOAD_MICROCODE, microcode) < 0) {
-			LOGE("Copying notification microcode failed - %d", errno);
+			ALOGE("Copying notification microcode failed - %d", errno);
 			ret = -errno;
 		}
 		if (ret == 0 && on) {
 			if (ioctl(fd, LM8502_START_ENGINE, 2) < 0) {
-				LOGE("Starting notification LED engine 2 failed - %d", errno);
+				ALOGE("Starting notification LED engine 2 failed - %d", errno);
 				ret = -errno;
 			}
 		}
 		if (ret == 0) {
 			if (ioctl(fd, LM8502_START_ENGINE, 1) < 0) {
-				LOGE("Starting notification LED engine 1 failed - %d", errno);
+				ALOGE("Starting notification LED engine 1 failed - %d", errno);
 				ret = -errno;
 			}
 		}
 		if (ret == 0 && !on) {
-		    LOGI("stop engine 1");
+		    ALOGI("stop engine 1");
 			if (ioctl(fd, LM8502_STOP_ENGINE, 1) < 0) {
-				LOGE("Stopping notification LED engine 1 failed - %d", errno);
+				ALOGE("Stopping notification LED engine 1 failed - %d", errno);
 				ret = -errno;
 			}
 			if (ioctl(fd, LM8502_STOP_ENGINE, 2) < 0) {
-				LOGE("Stopping notification LED engine 2 failed - %d", errno);
+				ALOGE("Stopping notification LED engine 2 failed - %d", errno);
 				ret = -errno;
 			}
 		}
 		if (ret == 0 && !on) {
-			LOGD("Waiting for notification LED engine to stop after reset");
+			ALOGD("Waiting for notification LED engine to stop after reset");
 			int state;
 			/* make sure the reset is complete */
 			if (ioctl(fd, LM8502_WAIT_FOR_ENGINE_STOPPED, &state) < 0) {
-				LOGW("Waiting for notification LED reset failed - %d", errno);
+				ALOGW("Waiting for notification LED reset failed - %d", errno);
 				ret = -errno;
 			}
-			LOGD("Notification LED reset finished with stop state %d", state);
+			ALOGD("Notification LED reset finished with stop state %d", state);
 		}
 
 		close(fd);
@@ -217,11 +217,11 @@ static int set_light_backlight(struct light_device_t *dev,
 
 	/* Tell touchscreen to turn on or off */
 	if (brightness > 0 && ts_state == 0) {
-		LOGI("Enabling touch screen");
+		ALOGI("Enabling touch screen");
 		ts_state = 1;
 		send_ts_socket("O");
 	} else if (brightness == 0 && ts_state == 1) {
-		LOGI("Disabling touch screen");
+		ALOGI("Disabling touch screen");
 		ts_state = 0;
 		send_ts_socket("C");
 	}
@@ -232,7 +232,7 @@ static int set_light_backlight(struct light_device_t *dev,
 
 static int close_lights(struct light_device_t *dev)
 {
-	LOGV("close_light is called");
+	ALOGV("close_light is called");
 	if (dev)
 		free(dev);
 
@@ -248,7 +248,7 @@ static int open_lights(const struct hw_module_t *module, char const *name,
 	int (*set_light)(struct light_device_t *dev,
 		struct light_state_t const *state);
 
-	LOGV("open_lights: open with %s", name);
+	ALOGV("open_lights: open with %s", name);
 
 	if (0 == strcmp(LIGHT_ID_BACKLIGHT, name))
 		set_light = set_light_backlight;
